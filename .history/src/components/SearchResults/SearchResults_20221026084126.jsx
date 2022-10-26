ac58ac16c72bd24df8ae8bb9ed3829975962d1af
haviles04 <haviles04@gmail.com>
2022-10-26 08:53:38 -0400

    inital file structure

---
+++ .history/src/components/SearchResults/SearchResults_20221026084126.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SearchResults() {
+  return (
+    <div className="SearchResults">
+  <h2>Results</h2>
+  <TrackList />
+</div>
+  )
+}
+
+export default SearchResults
